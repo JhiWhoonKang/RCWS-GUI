39af9a2812bff211fc59482e06c34b23cf5e5db3
JhiWhoon Kang <kangjw0914@naver.com>
2023-08-29 20:43:32 +0900

    add azimuth and elevation (AZEL)

--- RCWS_Situation-room/RCWS_Situation-room/GUI.Designer.cs
+++ RCWS_Situation-room/RCWS_Situation-room/GUI.Designer.cs
@@ -34,8 +34,15 @@
             this.pn_mapcontainer = new System.Windows.Forms.Panel();
             this.rtb_sendtcp = new System.Windows.Forms.RichTextBox();
             this.rtb_receivetcp = new System.Windows.Forms.RichTextBox();
+            this.Connect = new System.Windows.Forms.Button();
+            this.pictureBox_VIEW = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Map)).BeginInit();
             this.pn_mapcontainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_VIEW)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox_Map
@@ -85,10 +92,67 @@
             this.rtb_receivetcp.TabIndex = 4;
             this.rtb_receivetcp.Text = "";
             // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(13, 13);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(92, 29);
+            this.Connect.TabIndex = 5;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // pictureBox_VIEW
+            // 
+            this.pictureBox_VIEW.Location = new System.Drawing.Point(267, 49);
+            this.pictureBox_VIEW.Name = "pictureBox_VIEW";
+            this.pictureBox_VIEW.Size = new System.Drawing.Size(235, 247);
+            this.pictureBox_VIEW.TabIndex = 6;
+            this.pictureBox_VIEW.TabStop = false;
+            this.pictureBox_VIEW.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_VIEW_Paint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 116);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(93, 21);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(43, 171);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(93, 21);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "방위각";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "고각";
+            // 
             // GUI
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox_VIEW);
+            this.Controls.Add(this.Connect);
             this.Controls.Add(this.rtb_receivetcp);
             this.Controls.Add(this.rtb_sendtcp);
             this.Controls.Add(this.btn_close);
@@ -100,7 +164,9 @@
             this.Text = "MotionControl";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Map)).EndInit();
             this.pn_mapcontainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_VIEW)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -111,5 +177,11 @@
         private System.Windows.Forms.Panel pn_mapcontainer;
         private System.Windows.Forms.RichTextBox rtb_sendtcp;
         private System.Windows.Forms.RichTextBox rtb_receivetcp;
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.PictureBox pictureBox_VIEW;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- RCWS_Situation-room/RCWS_Situation-room/GUI.cs
+++ RCWS_Situation-room/RCWS_Situation-room/GUI.cs
@@ -18,23 +18,23 @@
     using System.Net.NetworkInformation;
 
     namespace RCWS_Situation_room
+{
+    public partial class GUI : Form
     {
-        public partial class GUI : Form
-        {
-            /*map*/
-            private Bitmap mapImage;
-            private float currentScale = 1.0f;
-            private float zoomFactor = 1.1f;
-            private bool isDragging = false;
-            private int lastX;
-            private int lastY;
+        /*map*/
+        private Bitmap mapImage;
+        private float currentScale = 1.0f;
+        private float zoomFactor = 1.1f;
+        private bool isDragging = false;
+        private int lastX;
+        private int lastY;
 
-            /*motion control*/
-            private StreamWriter _streamWriter;
-            private HashSet<Keys> _pressedKeys = new HashSet<Keys>();
+        /*motion control*/
+        private StreamWriter _streamWriter;
+        private HashSet<Keys> _pressedKeys = new HashSet<Keys>();
 
-            StreamReader streamReader;
-            StreamWriter streamWriter;
+        StreamReader streamReader;
+        StreamWriter streamWriter;
 
         public GUI()
         {
@@ -55,9 +55,9 @@
             this.KeyDown += new KeyEventHandler(MotionControl_KeyDown);
             this.KeyUp += new KeyEventHandler(MotionControl_KeyUp);
 
-            Thread _thread = new Thread(TcpConnect);
-            _thread.IsBackground = true;
-            _thread.Start();
+            //Thread _thread = new Thread(TcpConnect);
+            //_thread.IsBackground = true;
+            //_thread.Start();
         }
 
         private void UpdateMapImage()
@@ -72,14 +72,14 @@
             }
             pictureBox_Map.Image = resizedImage;
         }
-        
+
         private void MapPictureBox_MouseWheel(object sender, MouseEventArgs e)
         {
             if (e.Delta > 0)
                 currentScale *= zoomFactor;
             else
                 currentScale /= zoomFactor;
-            
+
             UpdateMapImage();
         }
 
@@ -101,22 +101,22 @@
                 {
                     int deltaX = e.X - lastX;
                     int deltaY = e.Y - lastY;
-                    
+
                     int map_newX = pictureBox_Map.Location.X + deltaX;
                     int map_newY = pictureBox_Map.Location.Y + deltaY;
-                    
+
                     pictureBox_Map.Location = new Point(map_newX, map_newY);
                     lastX = e.X;
                     lastY = e.Y;
                 }
             }
-            
+
             catch (Exception ex)
             {
                 MessageBox.Show("Error in MapPictureBox_MouseMove: " + ex.Message);
             }
         }
-        
+
         private void MapPictureBox_MouseUp(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
@@ -125,57 +125,57 @@
             }
         }
 
-        private void pictureBox_Map_Click(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
-                // 클릭한 위치의 좌표를 화면 좌표에서 이미지 좌표로 변환
-                int imageX = (int)(e.X / currentScale);
-                int imageY = (int)(e.Y / currentScale);
+        //private void pictureBox_Map_Click(object sender, MouseEventArgs e)
+        //{
+        //    if (e.Button == MouseButtons.Left)
+        //    {
+        //        // 클릭한 위치의 좌표를 화면 좌표에서 이미지 좌표로 변환
+        //        int imageX = (int)(e.X / currentScale);
+        //        int imageY = (int)(e.Y / currentScale);
 
-                // RCWS 위치로 표시
-                DrawRCWSLocation(imageX, imageY);
+        //        // RCWS 위치로 표시
+        //        DrawRCWSLocation(imageX, imageY);
 
-                // RCWS가 해당 위치를 바라보는 방향을 나타내는 막대 직선 그리기
-                DrawDirection(imageX, imageY);
-            }
-        }
+        //        // RCWS가 해당 위치를 바라보는 방향을 나타내는 막대 직선 그리기
+        //        DrawDirection(imageX, imageY);
+        //    }
+        //}
 
-        private void DrawRCWSLocation(int x, int y)
-        {
-            // RCWS 위치를 표시할 그래픽스 객체 생성
-            using (Graphics g = Graphics.FromImage(pictureBox_Map.Image))
-            {
-                // 원을 그려서 RCWS 위치를 나타냅니다.
-                int rcwsRadius = 10; // 원의 반지름
-                Pen pen = new Pen(Color.Red, 2); // 원의 색상과 선 두께 설정
-                g.DrawEllipse(pen, x - rcwsRadius, y - rcwsRadius, rcwsRadius * 2, rcwsRadius * 2);
-            }
+        //private void DrawRCWSLocation(int x, int y)
+        //{
+        //    // RCWS 위치를 표시할 그래픽스 객체 생성
+        //    using (Graphics g = Graphics.FromImage(pictureBox_Map.Image))
+        //    {
+        //        // 원을 그려서 RCWS 위치를 나타냅니다.
+        //        int rcwsRadius = 10; // 원의 반지름
+        //        Pen pen = new Pen(Color.Red, 2); // 원의 색상과 선 두께 설정
+        //        g.DrawEllipse(pen, x - rcwsRadius, y - rcwsRadius, rcwsRadius * 2, rcwsRadius * 2);
+        //    }
 
-            // 지도 이미지 업데이트
-            pictureBox_Map.Invalidate();
-        }
+        //    // 지도 이미지 업데이트
+        //    pictureBox_Map.Invalidate();
+        //}
 
-        private void DrawDirection(int rcwsX, int rcwsY)
-        {
-            // RCWS 위치에서 클릭한 위치까지의 선을 그리는 그래픽스 객체 생성
-            using (Graphics g = Graphics.FromImage(pictureBox_Map.Image))
-            {
-                Pen pen = new Pen(Color.Blue, 2); // 선의 색상과 선 두께 설정
+        //private void DrawDirection(int rcwsX, int rcwsY)
+        //{
+        //    // RCWS 위치에서 클릭한 위치까지의 선을 그리는 그래픽스 객체 생성
+        //    using (Graphics g = Graphics.FromImage(pictureBox_Map.Image))
+        //    {
+        //        Pen pen = new Pen(Color.Blue, 2); // 선의 색상과 선 두께 설정
 
-                // RCWS 위치에서 클릭한 위치까지 선 그리기
-                g.DrawLine(pen, rcwsX, rcwsY, lastX, lastY);
-            }
+        //        // RCWS 위치에서 클릭한 위치까지 선 그리기
+        //        g.DrawLine(pen, rcwsX, rcwsY, lastX, lastY);
+        //    }
 
-            // 지도 이미지 업데이트
-            pictureBox_Map.Invalidate();
-        }
+        //    // 지도 이미지 업데이트
+        //    pictureBox_Map.Invalidate();
+        //}
 
-        private void pictureBox_Map_Paint(object sender, PaintEventArgs e)
-        {
-            // PictureBox의 Paint 이벤트 핸들러에서 지도 이미지를 다시 그립니다.
-            e.Graphics.DrawImage(pictureBox_Map.Image, pictureBox_Map.Location);
-        }
+        //private void pictureBox_Map_Paint(object sender, PaintEventArgs e)
+        //{
+        //    // PictureBox의 Paint 이벤트 핸들러에서 지도 이미지를 다시 그립니다.
+        //    e.Graphics.DrawImage(pictureBox_Map.Image, pictureBox_Map.Location);
+        //}
 
         #region motion control
         private async void MotionControl_KeyDown(object sender, KeyEventArgs e)
@@ -183,17 +183,17 @@
             _pressedKeys.Add(e.KeyCode);
             await SendCommandStructure();
         }
-        
+
         private async void MotionControl_KeyUp(object sender, KeyEventArgs e)
         {
             _pressedKeys.Remove(e.KeyCode);
             await SendCommandStructure();
         }
-        
+
+        private bool ControlledByOperator = false;
+        Packet.SendTCP command = new Packet.SendTCP();
         private async Task SendCommandStructure()
         {
-            Packet.SendTCP command = new Packet.SendTCP();
-            
             if (_pressedKeys.Contains(Keys.A))
                 command.Pan = 1;
             if (_pressedKeys.Contains(Keys.D))
@@ -203,23 +203,25 @@
             if (_pressedKeys.Contains(Keys.S))
                 command.Tilt = (char)4;
             if (_pressedKeys.Contains(Keys.C))
+            {
                 command.Permission = 1;
-            
+                //ControlledByOperator = !ControlledByOperator;
+            }
             byte[] commandBytes = TcpReturn.StructToBytes(command);
             await _streamWriter.BaseStream.WriteAsync(commandBytes, 0, commandBytes.Length);
             await _streamWriter.BaseStream.FlushAsync();
         }
-        
+
+        Packet.ReceiveTCP receivedStruct;
         private void TcpConnect()
         {
             TcpClient tcpClient = new TcpClient();
-            
-            int TCPPORT = define.TCPPORT;
+
             try
             {
                 writeTcpRichTextbox("Connecting...");
-                tcpClient.Connect(define.SERVER_IP, TCPPORT);
-                
+                tcpClient.Connect(define.SERVER_IP, define.TCPPORT);
+
                 NetworkStream networkStream = tcpClient.GetStream();
                 streamReader = new StreamReader(networkStream);
                 streamWriter = new StreamWriter(networkStream);
@@ -230,29 +232,106 @@
                 writeTcpRichTextbox("Connect ERROR: " + ex.Message);
                 return;
             }
-            
+
             writeTcpRichTextbox("Server Connected");
-            
+
             try
             {
                 while (true)
                 {
                     byte[] receivedData = new byte[Marshal.SizeOf(typeof(Packet.ReceiveTCP))];
                     streamReader.BaseStream.Read(receivedData, 0, receivedData.Length);
-                    Packet.ReceiveTCP receivedStruct = TcpReturn.BytesToStruct<Packet.ReceiveTCP>(receivedData);
-                    
+
+                    receivedStruct = TcpReturn.BytesToStruct<Packet.ReceiveTCP>(receivedData);
+
                     writeTcpRichTextbox($"OpticalTilt: {receivedStruct.OpticalTilt}, OpticalPan: {receivedStruct.OpticalPan}, BodyTilt: {receivedStruct.BodyTilt}" +
                         $", BodyPan: {receivedStruct.BodyPan}, pointdistance: {receivedStruct.Permission}, Permission: {receivedStruct.Permission}");
                 }
             }
-            
+
             catch (Exception ex)
             {
                 writeTcpRichTextbox("Connect ERROR: " + ex.Message);
                 return;
             }
         }
-        
+
+        //private void View(Packet.ReceiveTCP ReceivedData)
+        //{
+        //    double _Pan, _Tilt;
+        //    if (ControlledByOperator)
+        //    {
+        //        _Pan = command.Pan; _Tilt=command.Tilt;
+        //    }
+        //    else
+        //    {
+        //        _Pan = receivedStruct.BodyPan; _Tilt=receivedStruct.BodyTilt;
+        //    }
+
+        //    Bitmap bmp = new Bitmap(pictureBox_VIEW.Width, pictureBox_VIEW.Height);
+
+        //    using (Graphics g = Graphics.FromImage(bmp))
+        //    {
+        //        g.Clear(Color.White);
+
+        //        int x2 = (int)(pictureBox_VIEW.Width / 2 + Math.Cos(_Pan) * pictureBox_VIEW.Width / 2);
+        //        int y2 = (int)(pictureBox_VIEW.Height / 2 - Math.Sin(_Pan) * pictureBox_VIEW.Height / 2);
+
+        //        Pen p = new Pen(Color.Black);
+        //        g.DrawLine(p, pictureBox_VIEW.Width / 2, pictureBox_VIEW.Height / 2, x2, y2);
+        //    }
+
+        //    pictureBox_VIEW.Image?.Dispose();
+        //    pictureBox_VIEW.Image = bmp;
+        //}
+
+        double currentRCWSDirection;
+        private void pictureBox_VIEW_Paint(object sender, PaintEventArgs e)
+        {
+            double angleInRadians = Math.PI * (currentRCWSDirection / 180.0);
+
+            int centerX = pictureBox_VIEW.Width / 2;
+            int centerY = pictureBox_VIEW.Height / 2;
+            int radius = Math.Min(centerX, centerY);
+
+            int lineX = centerX + (int)(radius * Math.Cos(angleInRadians));
+            int lineY = centerY - (int)(radius * Math.Sin(angleInRadians));
+
+            using (Pen pen = new Pen(Color.Black, 2))
+            {
+                e.Graphics.DrawLine(pen, centerX, centerY, lineX, lineY);
+            }
+
+            using (Pen redPen = new Pen(Color.Red, 2))
+            {
+                e.Graphics.DrawLine(redPen, centerX, centerY, lineX, lineY);
+            }
+        }
+
+        private double CalculateRCWSDirection(double bodyPan, double bodyTilt)
+        {
+
+            double directionInRadians = 0/*방위각, 고각 계산 여기*/ ;
+            return directionInRadians;
+        }
+
+        private void UpdateRCWSDirection()
+        {
+            double bodyPan = receivedStruct.BodyPan;
+            double bodyTilt = receivedStruct.BodyTilt;
+
+            currentRCWSDirection = CalculateRCWSDirection(bodyPan, bodyTilt);
+
+            pictureBox_VIEW.Invalidate();
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            Thread _thread = new Thread(TcpConnect);
+            _thread.IsBackground = true;
+            _thread.Start();
+        }
+
         private void writeTcpRichTextbox(string str)
         {
             rtb_sendtcp.Invoke((MethodInvoker)delegate { rtb_sendtcp.AppendText(str + "\r\n"); });
